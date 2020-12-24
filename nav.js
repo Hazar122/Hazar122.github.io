b94624db3307f0c65dc162566438cf243103bcd6
Hazar122 <harrisonodonnell66@gmail.com>
2020-12-24 03:46:01 +0000

    new nav

--- nav.js
+++ nav.js
@@ -1,35 +1,23 @@
-$(document).ready(function(){
-
-  var count = 0;
-
-  // Toggle Click
-  $("#menu").click(function(){
-    if(count==0)
-    {
-      $("#menu div:nth-child(1)").css("transform","matrix3d(-.71,.71,0,0,-.71,-.71,0,0,0,0,1,0,0,16,0,1)").css("width","53px");
-      $("#menu div:nth-child(3)").css("transform","matrix3d(-.71,-.71,0,0,.71,-.71,0,0,0,0,1,0,0,-16,0,1)").css("width","53px");
-      $("#menu div:nth-child(2)").css("opacity","0");
-      $("#nav a").css("transform","skew(10deg) translateX(100px)");
-      $("html, body").css("background","rgb(60,60,60)");
-      count = 1;
-    }
-    else if(count==1)
-    {
-      $("#menu div:nth-child(1)").css("transform","matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1)").css("width","47px");
-      $("#menu div:nth-child(3)").css("transform","matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1)").css("width","47px");
-      $("#menu div:nth-child(2)").css("opacity","1");
-      $("#nav a").css("transform","skew(10deg) translateX(350px)");
-      $("html, body").css("background","rgb(245,245,245)");
-      count = 0;
-    }
-    return count;
-  });
-
-  // Nav Hover
-  $("#nav a").hover(function(){
-      $(this).css("transform","skew(10deg) translateX(75px)");
-    }, function(){
-      $(this).css("transform","skew(10deg) translateX(100px)");
-  });
-
-});
+(function($) { // Begin jQuery
+  $(function() { // DOM ready
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      // Close one dropdown when selecting another
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    // Clicking away from dropdown will remove the dropdown class
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+    // Toggle open and close nav styles on click
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+    // Hamburger to X toggle
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+  }); // end DOM ready
+})(jQuery); // end jQuery
